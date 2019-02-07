694e2e2e7ef999aa6c5630bcf2a004e3457267c6
Jason Howmans <jason@nosaj.io>
2019-02-07 00:28:29 +0000

    Refactor data out of Front

--- src/components/Front/index.tsx
+++ src/components/Front/index.tsx
@@ -6,19 +6,7 @@ import Page from '../Page';
 import FrontProjects from './Projects';
 import { FrontMain, NosajIntro, WavesFixed } from './styled';
 import Thanks from './Thanks';
-
-const testimonials: BigTestimonialProps['quotes'] = [
-  {
-    quote:
-      '[Jason is] a one man startup machine - with a record of designing and building a huge range of websites from scratch...',
-    from: (
-      <>
-        Elliot O’Connor – Investor, <a href="">Firstminute Capital</a> &
-        Co-founder, <a href="">Code at Uni</a>
-      </>
-    )
-  }
-];
+import testimonials from './testimonials';
 
 export interface FrontProps {}
 
---
+++ src/components/Front/testimonials.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { BigTestimonialProps } from '../BigTestimonial';
+
+const testimonials: BigTestimonialProps['quotes'] = [
+  {
+    quote:
+      '[Jason is] a one man startup machine - with a record of designing and building a huge range of websites from scratch...',
+    from: (
+      <>
+        Elliot O’Connor – Investor, <a href="">Firstminute Capital</a> &
+        Co-founder, <a href="">Code at Uni</a>
+      </>
+    )
+  }
+];
+
+export default testimonials;
