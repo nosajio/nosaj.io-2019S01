b34c0bbb3107b46996a439ebf5b30501fd9691b5
Jason Howmans <jason@nosaj.io>
2019-03-07 16:35:42 +0000

    Fix waves bg on front page

--- src/components/Front/index.tsx
+++ src/components/Front/index.tsx
@@ -6,7 +6,7 @@ import Page from '../Page';
 import TopBar from '../TopBar';
 import testimonials from './data/testimonials';
 import FrontProjects from './Projects';
-import { FrontMain, NosajIntro, WavesFixed } from './styled';
+import { FrontMain, NosajIntro, FrontWaves } from './styled';
 
 export interface FrontProps {}
 
@@ -14,8 +14,8 @@ class Front extends React.Component<FrontProps, any> {
   public render() {
     return (
       <FrontMain>
+        <FrontWaves />
         <TopBar />
-        <WavesFixed />
         <Page.Section>
           <Opener headline="I help startups solve problems & forge great software" />
         </Page.Section>
--- src/components/Front/styled.ts
+++ src/components/Front/styled.ts
@@ -1,15 +1,17 @@
 import { styled, Theme } from '../../styled/theme';
 import Page from '../Page';
 import ProfileIntro from '../ProfileIntro';
-import Waves from '../Waves';
 
 export const FrontMain = styled(Page)``;
 
-export const WavesFixed = styled(Waves)`
-  z-index: ${({ theme }) => theme.layers.background};
+export const FrontWaves = styled.div`
+  background: no-repeat url('/bg/waves-front-grad.svg') top left / cover;
   position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: ${({ theme }) => theme.layers.background};
 `;
 
 export const NosajIntro = styled(ProfileIntro)``;
